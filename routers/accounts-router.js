1c41ae096e93dcff23c35ef448b19ca162662491
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-11-04 13:57:38 +0000

    delete added

--- routers/accounts-router.js
+++ routers/accounts-router.js
@@ -26,4 +26,31 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  db("accounts")
+    .insert({ name: req.body.name, budget: req.body.budget })
+    .then(result => {
+      res.status(201).json({
+        message: "Account number " + result + " successfully added",
+        account: { name: req.body.name, budget: req.body.budget }
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .del()
+    .then(result => {
+      res
+        .status(200)
+        .json({ message: "Number of accounts deleted: " + result });
+    })
+    .catch(err => {
+      res.status(500).json("Something went wrong " + err.message);
+    });
+});
+
+
+
 module.exports = router;
