2fc3d0a06bd0015a9f804ffdafd5f36b4f631175
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-11-04 13:39:31 +0000

    get account by id written

--- routers/accounts-router.js
+++ routers/accounts-router.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const db = require("../data/dbConfig");
 
@@ -12,6 +12,17 @@ router.get("/", (req, res) => {
     .catch(err => {
       console.log(err);
       res.status(500).json({ message: "Cannot get data: " + err.message });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Something went wrong: " + err.message });
     });
 });
 
